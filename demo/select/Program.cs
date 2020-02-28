591dcaf2045995588b9c86ed46d2364ee1126a3c
amosvoron <amos.voron@gmail.com>
2020-02-28 19:32:36 +0100

    Add querytalk demo db script, add first demo

---
+++ demo/select/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using QueryTalk;
+
+namespace QueryTalkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                d.SetConnection(@"Data Source=UNICE\SQL2016;Initial Catalog=QueryTalkBase;Integrated Security=True;");
+
+                var result = d.From("dbo.Person").Select()   
+                    .Go();
+            }
+            catch (QueryTalkException ex)
+            {
+                Console.WriteLine(ex.Report ?? ex.Message);
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                Console.WriteLine(String.Format("{0}: {1}", ex.Number, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException is QueryTalkException)
+                {
+                    Console.WriteLine(((QueryTalkException)ex.InnerException).Report);
+                }
+                else
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
