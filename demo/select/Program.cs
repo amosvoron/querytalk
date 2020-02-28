be190dd41a94608fb160c1b2c5e2f548d25e12ab
amosvoron <amos.voron@gmail.com>
2020-02-28 21:48:55 +0100

    chore: Replace wrong demo file

--- demo/select/Program.cs
+++ demo/select/Program.cs
@@ -7,35 +7,14 @@ namespace QueryTalkDemo
     {
         static void Main(string[] args)
         {
-            try
-            {
-                d.SetConnection(@"Data Source=UNICE\SQL2016;Initial Catalog=QueryTalkBase;Integrated Security=True;");
+            d.SetConnection(@"Data Source=UNICE\SQL2016;Initial Catalog=QueryTalkBase;Integrated Security=True;");
 
-                var result = d.From("dbo.Person").Select()   
-                    .Go();
-            }
-            catch (QueryTalkException ex)
-            {
-                Console.WriteLine(ex.Report ?? ex.Message);
-            }
-            catch (System.Data.SqlClient.SqlException ex)
-            {
-                Console.WriteLine(String.Format("{0}: {1}", ex.Number, ex.Message));
-            }
-            catch (Exception ex)
-            {
-                if (ex.InnerException is QueryTalkException)
-                {
-                    Console.WriteLine(((QueryTalkException)ex.InnerException).Report);
-                }
-                else
-                {
-                    Console.WriteLine(ex);
-                }
-            }
+            // Select all persons
+            var result = d.From("dbo.Person").Select()
+                .Test()
+                .Go();
 
             Console.ReadLine();
         }
-
     }
 }
